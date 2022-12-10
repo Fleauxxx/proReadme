638b9156904bc5beec93698df928d1bd850c3a74
Fleaux <hudsonfloyd@yahoo.com>
2022-12-10 13:06:15 -0600

    create template literal for user response and coded the fs.write file

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,11 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
 const inquirer = require('inquirer');
-const questions = [{
+
+// TODO: Create an array of questions for user input
+inquirer
+.prompt([
+  {
     type: 'input',
     message: 'What is the name of your project?',
     name: 'title',
@@ -8,33 +13,33 @@ const questions = [{
   
   {
     type: 'input',
-    message: 'Description',
+    message: 'Give a description of your project',
     name: 'description',
   },
   {
     type: 'input',
-    message: 'Installation Info',
-    name: 'install',
+    message: 'Is there any installation info',
+    name: 'installation',
   },
   {
     type: 'input',
-    message: 'Usage Info',
+    message: 'What is the usage info',
     name: 'usage',
   
   },
   {
     type: 'input',
-    message: 'Contribution Guidelines',
+    message: 'Are there any contribution guidelines',
     name: 'contributing',
   },
   {
     type: 'input',
-    message: 'Test Instructions',
-    name: 'testing',
+    message: 'Are there any test instructions',
+    name: 'tests',
 },
 {
     type: 'list',
-    message: 'Choose a License',
+    message: 'Choose a Github license',
     name: 'license',
     choices: ['apache license 2.0', 'Do What The F*ck You Want To Public License', 'Open Software License 3.0']
 },
@@ -48,19 +53,30 @@ const questions = [{
     message: 'Enter your email address',
     name: 'email',
 },
-];
+])
+.then((response) => {
+console.log("USER ANSWERS", response);
+const userAnswers = `Project Title: ${response.title}
+Project Description: ${response.description}
+Installation Info: ${response.install}
+Usage Info: ${response.usage}
+Contributing Guidelines: ${response.guidelines}
+Test Instructions: ${response.tests}
+Github License: ${response.license}
+Github Username: ${response.username}
+Email Address: ${response.email}`
 
-inquirer
-  .prompt(questions
-    
-  )
-//   .then((response) =>
-//     response.confirm === response.password
-//       ? console.log('Success!')
-//       : console.log('You forgot your password already?!')
-//   );
+fs.writeFile('readme.md', userAnswers, (error) => {
+if (error) {
+  console.log("Error:", error);
+} else {
+  console.log("Markdown Generated")
+}
+});
+});
 
-// TODO: Create an array of questions for user input
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
